62d2c435009614ff0faaf37a1fbbacc1aba06b99
firdoush <mdfirdoushali1776@gmail.com>
2024-08-09 21:16:24 +0530

    Added Home, Create comps

---
+++ TodoApp/src/Create.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Create(){
+    return(
+        <div>
+            <input type="text" name="" id="" />
+            <button type="submit">Add</button>
+        </div>
+    )
+}
+
+export default Create
